f9edf5c78b6116fa2241c6446a87cee2a9daea71
laladehulu <39814387+laladehulu@users.noreply.github.com>
2020-10-13 17:56:12 -0700

    a

--- public/assets/javascript/classroom.js
+++ public/assets/javascript/classroom.js
@@ -1,1 +1,200 @@
-var editor=ace.edit("editor",{mode:"ace/mode/javascript",selectionStyle:"text"});editor.setTheme("ace/theme/github"),editor.session.setMode("ace/mode/javascript");var EditSession=ace.require("ace/edit_session").EditSession,Range=ace.require("ace/range").Range;const questionTemplate='<div class="question-expand" onclick="expandQ(this)">\n{{status}}\n<div class="question-content">\n  <p class="question-content-text">\n    {{question}}\n  </p>\n  <span class="circle"></span>\n  <textarea></textarea>\n  <button class="btn-success btn" >Send</button>\n</div>\n</div>\n';editor.setReadOnly(!0);var code=editor.session.getDocument(),insertFromNetwork=(e,n)=>{1==n.length?insertChangeText():changeLines()};function createElementFromHTML(e){var n=document.createElement("div");return n.innerHTML=e.trim(),n.firstChild}const QuestionManager=function(){var e=document.querySelector(".right"),n=[];return{addQuestion:t=>{var o=createElementFromHTML(Mustache.render(questionTemplate,t));n.push(o);var s=n.length-1,a=o.querySelector(".question-content"),i=o.querySelector("textarea");o.querySelector("button").addEventListener("click",function(){console.log(i.value),sendAnswer(s,i.value),o.classList.toggle("question-active"),r.nodeValue="done",c.classList.add("circle-done")});var r=o.childNodes[0];r.nodeValue="Q: "+t.question.slice(0,30);var c=o.querySelector(".circle");a.querySelector("textarea").addEventListener("input",function(e){r.nodeValue="editing",c.classList.add("circle-editing")}),a.addEventListener("click",function(e){e.stopPropagation(),console.log("child clicked")}),e.appendChild(o)},removeQuestion:e=>{n[e].style.margin="0px",n[e].style.maxHeight="0",n[e].style.padding="0",console.log(n[e].style.transform);var t=n[e];setTimeout(()=>{t.remove()},500),n.splice(e,1)}}}();function handlerFunctions(){return{i:e=>{let n=e.split("/!");var t={row:parseInt(n[0],10),column:parseInt(n[1],10)};if(console.log(n),code.insert(t,n[2]),n.length>3){var o=n.slice(3);code.insertLines(t.row+1,o)}},l:e=>{let n={row:parseInt(e,10),column:0};code.insertNewLine(n)},r:e=>{let n=e.split("/!");var t={row:parseInt(n[0],10),column:parseInt(n[1],10)},o={row:parseInt(n[2],10),column:parseInt(n[3],10)},s=new Range(t.row,t.column,o.row,o.column);code.remove(s)},q:e=>{let n=e.split("/!");console.log(n[0]);var t={status:"unfinished",question:n[0]};QuestionManager.addQuestion(t)},p:e=>{let n=e.split("/!")[0];messageArray[0];QuestionManager.removeQuestion[n]},close:function(e){alert("the teacher has left the classroom")}}}const handlers=handlerFunctions();code.on("change",e=>{console.log(e)});var ws=new WebSocket("ws://"+window.location.hostname+":"+window.location.port),sendAnswer=function(e,n){ws.send("a"+e+"/!"+n+"/!"+name)};ws.onopen=function(){console.log(ClassCode),ws.send("student"),ws.send(ClassCode),ws.send("initiliazation sequence #3- apply all previous changes")},ws.onerror=function(e){console.log(e)},ws.onmessage=function(e){let n=e.data;console.log(n),handlers[n.charAt(0)](n.slice(1))};
+
+
+var editor = ace.edit("editor",{mode:"ace/mode/javascript",selectionStyle: "text"});
+editor.setTheme("ace/theme/github")
+editor.session.setMode("ace/mode/javascript");//initializing acer editor
+var EditSession = ace.require("ace/edit_session").EditSession;
+var Range= ace.require("ace/range").Range;
+
+const questionTemplate =  `<div class="question-expand" onclick="expandQ(this)">
+{{status}}
+<div class="question-content">
+  <p class="question-content-text">
+    {{question}}
+  </p>
+  <span class="circle"></span>
+  <textarea></textarea>
+  <button class="btn-success btn" >Send</button>
+</div>
+</div>
+`
+
+// and then to load document into editor, just call
+
+editor.setReadOnly(true);
+var code = editor.session.getDocument();
+
+
+var insertFromNetwork= (pos,text)=>{
+
+    if(text.length ==1){
+        insertChangeText();
+    }
+    else{
+        changeLines();
+    }
+}
+
+function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+  
+    // Change this to div.childNodes to support multiple top-level nodes
+    return div.firstChild; 
+  }
+const QuestionManager = (function(){
+    var RightSection = document.querySelector(".right");
+    var allQuestions = [];//made up of nodes
+
+ 
+    var addQuestion = (newQuestion)=>{
+        var htmlStr = Mustache.render(questionTemplate,newQuestion);//render and create the element
+        var ele = createElementFromHTML(htmlStr);
+        allQuestions.push(ele);
+        var index = allQuestions.length-1;
+        var content = ele.querySelector(".question-content");
+        var textArea = ele.querySelector("textarea");
+        var sendBtn = ele.querySelector("button");
+        sendBtn.addEventListener("click", function(){
+           console.log(textArea.value) ;
+           sendAnswer(index,textArea.value);
+           ele.classList.toggle("question-active")
+           finishStatus();
+        })
+        var statusNode= ele.childNodes[0];
+        statusNode.nodeValue = "Q: "+newQuestion.question.slice(0,30);
+        var statusCircle;
+        function  changeStatus() {
+            statusNode.nodeValue = "editing";
+            statusCircle.classList.add("circle-editing")
+        }
+        function finishStatus(){
+            statusNode.nodeValue = "done";
+            statusCircle.classList.add("circle-done")
+        }
+        var statusCircle = ele.querySelector(".circle")
+        content.querySelector("textarea").addEventListener('input', function (evt) {
+            changeStatus();
+            
+        });
+        content.addEventListener("click",function(event){
+            event.stopPropagation();
+            console.log("child clicked");
+        })
+        RightSection.appendChild(ele);
+    }
+    var removeQuestion = (index)=>{
+        allQuestions[index].style.margin = "0px";
+        allQuestions[index].style.maxHeight = "0";
+
+        allQuestions[index].style.padding = "0";
+        console.log( allQuestions[index].style.transform );
+        var toRemove = allQuestions[index];
+
+        setTimeout(() => {
+            toRemove.remove();//remove node
+        }, (500));
+
+        allQuestions.splice(index,1);//remove the null from index
+    }
+    return {addQuestion,removeQuestion};
+    
+})()
+
+
+
+
+
+
+function handlerFunctions(){
+    var insertChangeText = (content)=>{
+        let messageArray = content.split("/!");
+        var start = {row:parseInt(messageArray[0],10),column:parseInt(messageArray[1],10)};
+        //messageArray == [row,column,1st line,line,line]
+        console.log(messageArray);
+        code.insert(start,messageArray[2]);// add the first line to the starting pos
+        if(messageArray.length>3){
+        var lines = messageArray.slice(3);
+        code.insertLines(start.row+1,lines);
+        }
+    ///!
+    }
+    var insertChangeLine = (content)=>{
+        let place = {row:parseInt(content,10),column:0};
+        code.insertNewLine(place);
+    }
+    var removeChangeRange = (content)=>{
+        let messageArray = content.split("/!");
+        var start = {row:parseInt(messageArray[0],10),column:parseInt(messageArray[1],10)};
+        var end = {row:parseInt(messageArray[2],10),column:parseInt(messageArray[3],10)};
+        var range = new Range(start.row,start.column,end.row,end.column);
+        code.remove(range);
+
+    }
+    var addQuestion = (content)=>{
+        let messageArray = content.split("/!")
+        console.log(messageArray[0]);
+        var question = {status:"unfinished",question:messageArray[0]}
+        QuestionManager.addQuestion(question);  
+        
+
+    }
+    var removeQuestion = (content)=>{
+        let index = content.split("/!")[0]
+        var question = {status:"unfinished",question:messageArray[0]}
+        QuestionManager.removeQuestion[index];
+     
+
+    }
+    
+    
+    function teacherClose(content){
+        alert("the teacher has left the classroom");
+    }
+    var removeText = () =>{
+        
+    }
+    return  {
+        "i":insertChangeText,
+        "l":insertChangeLine,
+        "r":removeChangeRange,
+        'q':addQuestion,
+        'p':removeQuestion,
+        'close':teacherClose
+        
+    }
+}
+
+const handlers = handlerFunctions();
+
+
+code.on("change", (e)=>{
+    
+    console.log(e);
+})
+
+var ws = new WebSocket("ws://"+window.location.hostname
+            +':'+window.location.port
+            );
+var sendAnswer = function(index,answerText){
+    ws.send("a"+index+"/!"+answerText+"/!"+name);
+}
+
+ws.onopen= function(){
+    console.log(ClassCode);
+    ws.send("student");
+    ws.send(ClassCode);
+    ws.send("initiliazation sequence #3- apply all previous changes");
+
+}
+ws.onerror = function(e){
+    console.log(e);
+}
+
+ws.onmessage = function(e){
+    
+    let data = e.data
+    var pos, text;
+    console.log(data);
+    handlers[data.charAt(0)](data.slice(1));//use the first letter to distinguish message type, and pass the rest of the string to the handler function
+}
--- public/assets/javascript/t-classroom.js
+++ public/assets/javascript/t-classroom.js
@@ -1,1 +1,177 @@
-var editor=ace.edit("editor",{mode:"ace/mode/javascript",selectionStyle:"text"});editor.setTheme("ace/theme/github"),editor.session.setMode("ace/mode/javascript");var EditSession=ace.require("ace/edit_session").EditSession,Range=ace.require("ace/range").Range,js=new EditSession("some js code"),questionTemplate='<div class="question-expand" onclick="expandQ(this)">\n<p class="question-content-text">\n  \n</p>\n<p class="question-content-count">\n  Submitted #: <span class="count">1</span>\n</p>\n<div class="question-answers">\n  <div class="answer-container">\n   \n    \n  </div>\n \n</div>\n</div>',answerTemplate='         \n\n  <div class="answer">\n    <p class="answer-student-name">\n      Devin\n    </p>\n    <div class="answer-text">\n      Hi\n\n  </div>\n  \n</div>\n\n</div>\n\n';function createElementFromHTML(e){var n=document.createElement("div");return n.innerHTML=e.trim(),n.firstChild}var Question=function(){const e=function(e,n,t){this.answerText=n,this.studentName=t,this.element=createElementFromHTML(answerTemplate),i[e].element.querySelector(".answer-container").appendChild(this.element),console.log(this.element),this.studentNameDiv=this.element.querySelector(".answer-student-name"),this.textDiv=this.element.querySelector(".answer-text"),this.changeAnswerView(n,t)};e.prototype.changeAnswerView=function(e,n){this.studentNameDiv.innerHTML=n,this.textDiv.innerHTML=e};var n=document.querySelector(".questions"),t=document.querySelector(".add-question"),s=t.querySelector("textarea");t.style.display="none";var i=[];return{enableAddQuestionUI:e=>{t.style.display="block"},turnOffAddQuestionUI:e=>{t.style.display="none"},addQuestion:()=>{var e=s.value;let t=createElementFromHTML(questionTemplate);i.push({questionText:e,answers:{},element:t}),sendNewQuestion(e),n.appendChild(t),t.querySelector(".question-content-text").innerHTML=e,s.value=""},addAnswer:(n,t,s)=>{i[n].answers[s]?i[n].answers[s].changeAnswerView(t,s):i[n].answers[s]=new e(n,t,s)}}}(),code=editor.session.getDocument();function reformatData(e){return"insert"==e.action?function(e){var n="i"+e.start.row.toString()+"/!"+e.start.column.toString();return e.lines.forEach(e=>{n+="/!"+e}),console.log(n),n}(e):function(e){return"r"+e.start.row.toString()+"/!"+e.start.column.toString()+"/!"+e.end.row.toString()+"/!"+e.end.column.toString()}(e)}var ws=new WebSocket("ws://"+window.location.hostname+":"+window.location.port);function sendNewQuestion(e){ws.send("q"+e)}code.on("change",e=>{ws.send(reformatData(e))}),ws.onopen=function(){ws.send("teacher"),ws.send(ClassCode),ws.send("initiliazation sequence #3- apply all previous changes")};const Handlers=function(){return{a:function(e){let n=e.split("/!");Question.addAnswer(n[0],n[1],n[2])}}}();ws.onmessage=function(e){let n=e.data;Handlers[n.charAt(0)](n.slice(1))};
+
+
+var editor = ace.edit("editor",{mode:"ace/mode/javascript",selectionStyle: "text"});
+editor.setTheme("ace/theme/github")
+editor.session.setMode("ace/mode/javascript");//initializing acer editor
+var EditSession = ace.require("ace/edit_session").EditSession;
+var Range= ace.require("ace/range").Range;
+var js = new EditSession("some js code");
+
+var questionTemplate = `<div class="question-expand" onclick="expandQ(this)">
+<p class="question-content-text">
+  
+</p>
+<p class="question-content-count">
+  Submitted #: <span class="count">1</span>
+</p>
+<div class="question-answers">
+  <div class="answer-container">
+   
+    
+  </div>
+ 
+</div>
+</div>`
+var answerTemplate = `         
+
+  <div class="answer">
+    <p class="answer-student-name">
+      Devin
+    </p>
+    <div class="answer-text">
+      Hi
+
+  </div>
+  
+</div>
+
+</div>
+
+`
+function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+  
+    // Change this to div.childNodes to support multiple top-level nodes
+    return div.firstChild; 
+  }
+
+// and then to load document into editor, just call
+var Question = (function(){
+    const Answer = function(index, answerText,studentName){
+        this.answerText = answerText;
+        this.studentName = studentName;
+        this.element = createElementFromHTML(answerTemplate);
+        
+        allQuestions[index].element.querySelector(".answer-container").appendChild(this.element);//it need the index to add the element into the page
+        console.log(this.element);
+        this.studentNameDiv = this.element.querySelector(".answer-student-name");
+
+        this.textDiv = this.element.querySelector(".answer-text");
+        this.changeAnswerView(answerText,studentName);
+
+    }
+    Answer.prototype.changeAnswerView = function(answerText,studentName){
+  
+        this.studentNameDiv.innerHTML=studentName;
+ 
+        this.textDiv.innerHTML = answerText;
+    }
+    var QuestionSection = document.querySelector(".questions");
+
+    var QuestionAddInputEle = document.querySelector(".add-question");
+    var questionTextarea = QuestionAddInputEle.querySelector("textarea");
+    QuestionAddInputEle.style.display ="none";
+    var allQuestions = [];//{questionString,node}
+
+
+ 
+    var enableAddQuestionUI = (newQuestion)=>{
+        QuestionAddInputEle.style.display ="block";
+        
+    }
+    var turnOffAddQuestionUI = (newQuestion)=>{
+        QuestionAddInputEle.style.display ="none";
+        
+    }
+    var addQuestion = ()=>{
+        var questionText = questionTextarea.value;
+        let newQuestionEle = createElementFromHTML(questionTemplate);
+        allQuestions.push({questionText,answers:{},element:newQuestionEle});
+
+        sendNewQuestion(questionText);
+        QuestionSection.appendChild(newQuestionEle);
+        newQuestionEle.querySelector(".question-content-text").innerHTML = questionText;
+        questionTextarea.value = "";
+        
+    }
+    var addAnswer = (index, answerText,studentName)=>{
+        if(allQuestions[index].answers[studentName]){//if the student already responded, simply change the view by calling changeView function
+            allQuestions[index].answers[studentName].changeAnswerView(answerText,studentName);
+        }
+        else{
+            allQuestions[index].answers[studentName] = new Answer(index,answerText,studentName);
+        }
+
+       
+        allQuestions
+    }
+    return {
+        enableAddQuestionUI,turnOffAddQuestionUI,addQuestion,addAnswer
+        
+    }
+})()
+var code = editor.session.getDocument();
+
+
+function reformatData(change){
+    function formatInsert(change){
+        var str ='i'+change.start.row.toString()+'/!'+change.start.column.toString();
+        change.lines.forEach(element => {
+            str+= '/!'+element;
+        });
+        console.log(str);
+        return str;
+    }
+    function formatRemove(change){
+        var str ='r'+change.start.row.toString()+'/!'+change.start.column.toString()
+        +'/!'+change.end.row.toString()+'/!'+change.end.column.toString();
+        return str;
+    }
+    return change.action == "insert"?formatInsert(change): formatRemove(change);
+}
+
+var ws = new WebSocket("ws://"+window.location.hostname
+            +':'+window.location.port
+            );
+            function sendNewQuestion(questionText){
+                ws.send("q"+questionText)
+            }
+code.on("change", (e)=>{
+    ws.send(reformatData(e));
+    
+})
+
+
+ws.onopen= function(){
+    ws.send("teacher");
+    ws.send(ClassCode);
+    ws.send("initiliazation sequence #3- apply all previous changes");
+}
+const Handlers = (function handlerFunctions(){
+  
+    function answerQuestion(content){
+        let messageArray = content.split("/!");
+        Question.addAnswer(messageArray[0],messageArray[1],messageArray[2])
+    }
+    
+    function teacherClose(content){
+        
+    }
+    var removeText = () =>{
+        
+    }
+    return  {
+        'a':answerQuestion
+        
+    }
+})();
+
+ws.onmessage = function(e){
+    if(e =="e"){
+        alert("you are trying to create a room that already exist.Please open another instance.");
+    }
+   let data = e.data
+   //var pos, text;
+    Handlers[data.charAt(0)](data.slice(1));//use the first letter to distinguish message type, and pass the rest of the string to the handler function
+}
